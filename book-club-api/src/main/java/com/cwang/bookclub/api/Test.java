f12f64e83c66f14c6e24b6e10cf65d1ed6134822
Cassie Wang <cassiewang@Cassies-MacBook-Pro.local>
2018-01-14 15:12:37 -0800

    Example immutable up and running.

--- book-club-api/src/main/java/com/cwang/bookclub/api/Test.java
+++ book-club-api/src/main/java/com/cwang/bookclub/api/Test.java
@@ -1,8 +1,16 @@
 package com.cwang.bookclub.api;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@JsonSerialize(as = ImmutableTest.class)
+@JsonDeserialize(as = ImmutableTest.class)
 public interface Test {
+
+    String test();
+
+    String otherTest();
 
 }
