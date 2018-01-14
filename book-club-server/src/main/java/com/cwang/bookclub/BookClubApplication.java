c49b35e475d8389d71e6f16d8a498ad80719e36d
Cassie Wang <cassiewang@Cassies-MacBook-Pro.local>
2018-01-14 15:22:28 -0800

    Add dropwizard code.

--- book-club-server/src/main/java/com/cwang/bookclub/BookClubApplication.java
+++ book-club-server/src/main/java/com/cwang/bookclub/BookClubApplication.java
@@ -1,9 +1,27 @@
 package com.cwang.bookclub;
 
-public class BookClubApplication {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class BookClubApplication extends Application<BookClubConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new BookClubApplication().run(args);
     }
 
+    @Override
+    public String getName() {
+        return "Book Club Application";
+    }
+
+    @Override
+    public void initialize(Bootstrap<BookClubConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(BookClubConfiguration configuration, Environment environment) throws Exception {
+        System.out.println("Hello, World!");
+    }
 }
---
+++ book-club-server/src/main/java/com/cwang/bookclub/BookClubConfiguration.java
@@ -0,0 +1,7 @@
+package com.cwang.bookclub;
+
+import io.dropwizard.Configuration;
+
+public class BookClubConfiguration extends Configuration {
+
+}
