cdd51f1337035c3b9f830e7309b5fb616709dad2
Cassie Wang <cassie.e.wang@gmail.com>
2018-01-18 15:10:44 -0800

    Cassie's fix.

--- book-club-server/src/main/java/com/cwang/bookclub/resource/ClubResource.java
+++ book-club-server/src/main/java/com/cwang/bookclub/resource/ClubResource.java
@@ -1,11 +1,9 @@
+package com.cwang.bookclub.resource;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
-import java.util.ArrayList;
-import javax.ws.rs.PathParam;
-
 
 @Path("/club")
 @Produces(MediaType.APPLICATION_JSON)
